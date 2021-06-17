33bc4ec283281878ad13ec682c2d96ba30462b0f
Norbert Funke <norbert@funke.com>
2021-06-17 09:32:43 -0500

    updated layer version

--- pre-processing/pre-processing-code/wrapper.py
+++ pre-processing/pre-processing-code/wrapper.py
@@ -1,5 +1,7 @@
 import source_data
 from datetime import date, datetime
+import pandas as pd
+import openpyxl
 
 if __name__ == '__main__':
 
@@ -12,6 +14,20 @@ if __name__ == '__main__':
     AWS_SERVER_PUBLIC_KEY = lines[1].rstrip()
     AWS_SERVER_SECRET_KEY = lines[2].rstrip()
     RUN_LOCAL = True
+
+
+#    source_dataset_url = "https://pasteur.epa.gov/uploads/10.23719/1517796/SupplyChainEmissionFactorsforUSIndustriesCommodities.xlsx"
+#    df = pd.DataFrame.from_dict(pd.read_excel(source_dataset_url, sheet_name=None, engine='openpyxl'), orient='index', columns=['sheet','data'])
+#    df = pd.read_excel(source_dataset_url, sheet_name=None, engine='openpyxl')
+
+    #for row in df.rows:
+    #    print(row)
+
+    #df.columns.values[0] = 'Sheet'
+#    for keys in df.keys():
+#        print (keys)
+#        df[keys].to_csv('/Users/nfunke/Temp/' %keys)
+#    isempty = df.empty
 
     asset_list = source_data.source_dataset(
         #"world-bank-cpi.csv", "norbert-adx-test2", AWS_SERVER_SECRET_KEY
